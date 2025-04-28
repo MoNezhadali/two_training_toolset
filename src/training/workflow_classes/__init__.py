762528e98506438e32d3a5442fa7529298eb3a89
MoNezhadali <monezhadali@gmail.com>
2025-04-28 23:10:01 +0200

    Add some more __init__ files

---
+++ src/training/workflow_classes/__init__.py
@@ -0,0 +1,8 @@
+from src.training.workflow_classes.iris_classifier import (
+    IrisClassifier,
+)
+from src.training.workflow_classes.workflow_tempalate import (
+    WorkflowTemplate,
+)
+
+__all__ = ["IrisClassifier", "WorkflowTemplate"]
