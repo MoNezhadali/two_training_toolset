16af792d57468f42e2ecdd563aa18498e80b000a
MoNezhadali <monezhadali@gmail.com>
2025-04-29 16:57:21 +0200

    Add dummy authentication

---
+++ src/serve/api_utils/authentication.py
@@ -0,0 +1,16 @@
+"""Module for authentication utilities."""
+
+
+# Dummy authenticator
+def dummy_authenticator() -> bool:
+    """
+    Dummy authenticator function that always returns True.
+
+    Args:
+    ----
+        None
+    Returns:
+        bool: Always returns True.
+
+    """
+    return True
