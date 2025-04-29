235e7d919642436c9461617db0d21a0addb4a05a
MoNezhadali <monezhadali@gmail.com>
2025-04-29 17:02:22 +0200

    Add pydantic schemas for both api call and api response payloads

---
+++ src/serve/api_utils/schemas.py
@@ -0,0 +1,27 @@
+"""
+This module contains the schemas for the API requests and responses.
+It defines the request and response models using Pydantic.
+"""
+
+from typing import Literal
+
+from pydantic import BaseModel, Field
+
+
+class IrisRequest(BaseModel):
+    """Request model for predicting Iris species."""
+
+    request_id: str
+    sepal_length: float = Field(..., ge=0)
+    sepal_width: float = Field(..., ge=0)
+    petal_length: float = Field(..., ge=0)
+    petal_width: float = Field(..., ge=0)
+
+
+class IrisResponse(BaseModel):
+    """Response model for Iris species prediction."""
+
+    prediction: int
+    prediction_label: Literal["setosa", "versicolor", "virginica"]
+    request_id: str
+    model_version: str
